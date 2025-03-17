6cde4c4cd76c47fdeb028ef39bb77456babdac3e
Nam Le Hoang <lehoangnam.31123@gmail.com>
2025-03-17 23:07:52 +0700

    style(ProductDetail): style layout and add img into UI Product Detail

--- src/pages/products/ProductDetail.tsx
+++ src/pages/products/ProductDetail.tsx
@@ -8,12 +8,24 @@ const ProductDetail = () => {
   debug();
   return (
     <div className="container product-detail d-flex flex-column">
-      <div>Name: {product.name}</div>
-      <div>Base price: {product.basePrice}</div>
-      <div>Current price: {product.currentPrice}</div>
-      <div>Cost Price: {product.costPrice}</div>
-      <div>Description: {product.description}</div>
-      <div>Total Quantity: {product.totalQuantity}</div>
+      <div className="row py-3">
+        <div className="col-6">
+          <img
+            width="100%"
+            src={AssetsConstant.PRODUCTS.IPHONE_16_PRO_MAX}
+            alt=""
+            style={{ borderRadius: "10px" }}
+          />
+        </div>
+        <div className="col-6">
+          <div>Name: {product.name}</div>
+          <div>Base price: {product.basePrice}</div>
+          <div>Current price: {product.currentPrice}</div>
+          <div>Cost Price: {product.costPrice}</div>
+          <div>Description: {product.description}</div>
+          <div>Total Quantity: {product.totalQuantity}</div>
+        </div>
+      </div>
     </div>
   );
 };
